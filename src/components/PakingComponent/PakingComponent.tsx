5b3cea329ad0a39f969ad152bbeebf5d051deba0
Roman <mazaxaka.tyt@gmail.com>
2023-10-18 16:58:01 +0300

    add PakingComponent

---
+++ src/components/PakingComponent/PakingComponent.tsx
@@ -0,0 +1,32 @@
+import "./PakingComponent.scss";
+
+type Props = {};
+
+const PakingComponent = (props: Props) => {
+    return (
+        <section className="paking-section">
+            <div className="small-container">
+                <p className="subtitle">Our Packaging</p>
+                <p className="title">Favellini</p>
+                <p className="text">
+                    All Favellini products are packaged in a gift box.
+                </p>
+                <p className="text">
+                    This box is convenient for storing laundry at home.
+                </p>
+                <img
+                    className="paking-img-1"
+                    src="images/innovation-img-1.jpg"
+                    alt=""
+                />
+                <img
+                    className="paking-img-2"
+                    src="images/innovation-img-2.jpg"
+                    alt=""
+                />
+            </div>
+        </section>
+    );
+};
+
+export default PakingComponent;
--- src/pages/CollectionPage/CollectionPage.tsx
+++ src/pages/CollectionPage/CollectionPage.tsx
@@ -1,9 +1,17 @@
+import PakingComponent from "../../components/PakingComponent/PakingComponent";
 import "./CollectionPage.scss";
 
 type Props = {};
 
 const CollectionPage = (props: Props) => {
-    return <div>CollectionPage</div>;
+    return (
+        <main className="main">
+            <div className="collection-page">
+                <div className="header-bg"></div>
+                <PakingComponent />
+            </div>
+        </main>
+    );
 };
 
 export default CollectionPage;
