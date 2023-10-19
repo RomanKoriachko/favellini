e1011b8b822807f420f7733c2e2f9167273e6330
Roman <mazaxaka.tyt@gmail.com>
2023-10-19 11:32:27 +0300

    create item page in PagesNavigation

--- src/components/PagesNavigation/PagesNavigation.tsx
+++ src/components/PagesNavigation/PagesNavigation.tsx
@@ -1,18 +1,54 @@
+import itemsArray from "../../pages/CollectionPage/itemsArray";
 import "./PagesNavigation.scss";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 type Props = {};
 
 const PagesNavigation = (props: Props) => {
+    const { color } = useParams();
+    let currentItem;
+    for (let i = 0; i < itemsArray.length; i++) {
+        if (itemsArray[i].color === color) {
+            currentItem = itemsArray[i];
+        }
+    }
+
+    let curentPageClass = {
+        currentCollection: "current-page",
+        currentItem: "current-page",
+    };
+
+    if (color) {
+        curentPageClass.currentCollection = "";
+        curentPageClass.currentItem = "current-page";
+    } else {
+        curentPageClass.currentCollection = "current-page";
+        curentPageClass.currentItem = "";
+    }
+
     return (
         <div className="row pages">
             <Link className="page-text" to="/">
                 <p>Home</p>
             </Link>
             <div className="page-text">{">"}</div>
-            <Link className="page-text current-pages" to="/collection">
+            <Link
+                className={`page-text ${curentPageClass.currentCollection}`}
+                to="/collection"
+            >
                 <p>Collection</p>
             </Link>
+            {currentItem ? (
+                <>
+                    <div className="page-text">{">"}</div>
+                    <Link
+                        className={`page-text ${curentPageClass.currentItem}`}
+                        to={`/collection/${color}`}
+                    >
+                        <p>{color}</p>
+                    </Link>
+                </>
+            ) : undefined}
         </div>
     );
 };
--- src/pages/ItemPage/ItemPage.tsx
+++ src/pages/ItemPage/ItemPage.tsx
@@ -1,16 +1,26 @@
 import { useParams } from "react-router-dom";
 import "./ItemPage.scss";
 import HistoryComponent from "../../components/HistoryComponent/HistoryComponent";
+import PagesNavigation from "../../components/PagesNavigation/PagesNavigation";
+import itemsArray from "../CollectionPage/itemsArray";
 
 type Props = {};
 
 const ItemPage = (props: Props) => {
     const { color } = useParams();
+    let currentItem;
+    for (let i = 0; i < itemsArray.length; i++) {
+        if (itemsArray[i].color === color) {
+            currentItem = itemsArray[i];
+        }
+    }
     return (
         <main className="main">
             <div className="item-page">
                 <div className="header-bg"></div>
-                {color}
+                <div className="container">
+                    <PagesNavigation />
+                </div>
                 <HistoryComponent />
             </div>
         </main>
