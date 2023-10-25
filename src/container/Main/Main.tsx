b92ff138c58d4534cbb02b3c603264efa20315fa
Roman <mazaxaka.tyt@gmail.com>
2023-10-25 16:38:18 +0300

    add react Helmet provider

--- src/container/Main/Main.tsx
+++ src/container/Main/Main.tsx
@@ -5,19 +5,29 @@ import HistoryComponent from "../../components/HistoryComponent/HistoryComponent
 import InnovationComponent from "../../components/InnovationComponent/InnovationComponent";
 import VideoComponent from "../../components/VideoComponent/VideoComponent";
 import "./Main.scss";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 type Props = {};
 
 const Main = (props: Props) => {
     return (
-        <main className="main">
-            <VideoComponent />
-            <AboutComponent />
-            <ExploreComponent />
-            <HistoryComponent />
-            <InnovationComponent />
-            <FeedbackComponent />
-        </main>
+        <HelmetProvider>
+            <main className="main">
+                <Helmet>
+                    <title>Favellini</title>
+                    <meta
+                        name="description"
+                        content="Founded by the visionary designer, Roberto Favellini, our brand has redefined opulence and perfection. We believe that luxury should be an everyday experience, and our mission is to bring unparalleled comfort to your life."
+                    />
+                </Helmet>
+                <VideoComponent />
+                <AboutComponent />
+                <ExploreComponent />
+                <HistoryComponent />
+                <InnovationComponent />
+                <FeedbackComponent />
+            </main>
+        </HelmetProvider>
     );
 };
 
