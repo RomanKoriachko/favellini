70dbc1e3665c3575b39040af8af264060e8bac14
Roman <mazaxaka.tyt@gmail.com>
2023-10-26 13:59:43 +0300

    add SortingComponent

---
+++ src/components/SortingComponent/SortingComponent.tsx
@@ -0,0 +1,166 @@
+import "./SortingComponent.scss";
+import { useState, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../redux/hooks";
+import { setItemsState } from "../../redux/itemsReducer";
+import itemsArray from "../../pages/CollectionPage/itemsArray";
+import { setSortingTitleState } from "../../redux/sortingReducer";
+
+type Props = {};
+
+const SortingComponent = (props: Props) => {
+    const itemsArrState = useAppSelector((state) => state.itemsState);
+    const sortingTitleState = useAppSelector((state) => state.sortingState);
+    const dispatch = useAppDispatch();
+
+    // Open Sorting menu
+
+    const [sortingState, setSortingState] = useState("");
+    const openSortingList = () => {
+        sortingState === "" ? setSortingState("open") : setSortingState("");
+    };
+
+    // Sorting
+
+    useEffect(() => {
+        let availableItems = itemsArrState.filter((item) => item.inStock);
+        let unavailableItems = itemsArrState.filter((item) => !item.inStock);
+
+        const sortedAvailableItems = sortByField(
+            availableItems,
+            "inStock",
+            false
+        );
+        const sortedUnavailableItems = sortByField(
+            unavailableItems,
+            "inStock",
+            false
+        );
+
+        const sortedItems = [
+            ...sortedAvailableItems,
+            ...sortedUnavailableItems,
+        ];
+
+        dispatch(setItemsState(sortedItems));
+        // eslint-disable-next-line
+    }, []);
+
+    function sortByField<T>(array: T[], field: keyof T, ascending = true): T[] {
+        return [...array].sort((a, b) => {
+            if (ascending) {
+                return (a[field] as any) - (b[field] as any);
+            } else {
+                return (b[field] as any) - (a[field] as any);
+            }
+        });
+    }
+
+    const handleSortByField = (
+        field: keyof (typeof itemsArray)[0],
+        ascending = true
+    ) => {
+        let availableItems = itemsArrState.filter((item) => item.inStock);
+        let unavailableItems = itemsArrState.filter((item) => !item.inStock);
+
+        const sortedAvailableItems = sortByField(
+            availableItems,
+            field,
+            ascending
+        );
+        const sortedUnavailableItems = sortByField(
+            unavailableItems,
+            field,
+            ascending
+        );
+
+        const sortedItems = [
+            ...sortedAvailableItems,
+            ...sortedUnavailableItems,
+        ];
+
+        dispatch(setItemsState(sortedItems));
+    };
+
+    const sortByLowerPrice = () => {
+        handleSortByField("queensPrice", true);
+        dispatch(setSortingTitleState("From cheap to expensive"));
+        setSortingState("");
+    };
+    const sortByHighestPrice = () => {
+        handleSortByField("queensPrice", false);
+        dispatch(setSortingTitleState("From expensive to cheap"));
+        setSortingState("");
+    };
+    const sortByPopularity = () => {
+        handleSortByField("popularity", false);
+        dispatch(setSortingTitleState("By popularity"));
+        setSortingState("");
+    };
+    const sortByStock = () => {
+        handleSortByField("inStock", false);
+        dispatch(setSortingTitleState("In stock"));
+        setSortingState("");
+    };
+    const sortByDefault = () => {
+        handleSortByField("id", true);
+        dispatch(setSortingTitleState("Default"));
+        setSortingState("");
+    };
+
+    return (
+        <div className="sorting">
+            <div className="row sorting-row" onClick={openSortingList}>
+                <p>Sorting: {sortingTitleState}</p>
+                <div className={`arrow ${sortingState}`}></div>
+            </div>
+            <div className={`sorting-list ${sortingState}`}>
+                <p
+                    className={`${
+                        sortingTitleState === "From expensive to cheap"
+                            ? "disable"
+                            : ""
+                    }`}
+                    onClick={sortByHighestPrice}
+                >
+                    From expensive to cheap
+                </p>
+                <p
+                    className={`${
+                        sortingTitleState === "From cheap to expensive"
+                            ? "disable"
+                            : ""
+                    }`}
+                    onClick={sortByLowerPrice}
+                >
+                    From cheap to expensive
+                </p>
+                <p
+                    className={`${
+                        sortingTitleState === "By popularity" ? "disable" : ""
+                    }`}
+                    onClick={sortByPopularity}
+                >
+                    By popularity
+                </p>
+                <p
+                    className={`${
+                        sortingTitleState === "In stock" ? "disable" : ""
+                    }`}
+                    onClick={sortByStock}
+                >
+                    In stock
+                </p>
+                <p
+                    className={`${
+                        sortingTitleState === "Default" ? "disable" : ""
+                    }`}
+                    onClick={sortByDefault}
+                >
+                    Default
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default SortingComponent;
--- src/pages/CollectionPage/CollectionPage.tsx
+++ src/pages/CollectionPage/CollectionPage.tsx
@@ -1,122 +1,19 @@
 import ItemComponent from "../../components/ItemComponent/ItemComponent";
 import PagesNavigation from "../../components/PagesNavigation/PagesNavigation";
 import PakingComponent from "../../components/PakingComponent/PakingComponent";
-import itemsArray from "./itemsArray";
 import "./CollectionPage.scss";
 import HistoryComponent from "../../components/HistoryComponent/HistoryComponent";
-import { useState, useEffect } from "react";
 import "rc-slider/assets/index.css";
 import StickyBox from "react-sticky-box";
 import { Helmet } from "react-helmet-async";
 import FilterComponent from "../../components/FilterComponent/FilterComponent";
-import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import { setItemsState } from "../../redux/itemsReducer";
-import { setSortingTitleState } from "../../redux/sortingReducer";
+import SortingComponent from "../../components/SortingComponent/SortingComponent";
+import { useAppSelector } from "../../redux/hooks";
 
 type Props = {};
 
 const CollectionPage = (props: Props) => {
     const itemsArrState = useAppSelector((state) => state.itemsState);
-    const sortingTitleState = useAppSelector((state) => state.sortingState);
-    const dispatch = useAppDispatch();
-
-    // Open Sorting menu
-
-    const [sortingState, setSortingState] = useState("");
-    const openSortingList = () => {
-        sortingState === "" ? setSortingState("open") : setSortingState("");
-    };
-
-    // Sorting
-
-    useEffect(() => {
-        let availableItems = itemsArrState.filter((item) => item.inStock);
-        let unavailableItems = itemsArrState.filter((item) => !item.inStock);
-
-        const sortedAvailableItems = sortByField(
-            availableItems,
-            "inStock",
-            false
-        );
-        const sortedUnavailableItems = sortByField(
-            unavailableItems,
-            "inStock",
-            false
-        );
-
-        const sortedItems = [
-            ...sortedAvailableItems,
-            ...sortedUnavailableItems,
-        ];
-
-        dispatch(setItemsState(sortedItems));
-        // eslint-disable-next-line
-    }, []);
-
-    // const [sortingTitleState, setSortingTitleState] =
-    //     useState<string>("Default");
-
-    function sortByField<T>(array: T[], field: keyof T, ascending = true): T[] {
-        return [...array].sort((a, b) => {
-            if (ascending) {
-                return (a[field] as any) - (b[field] as any);
-            } else {
-                return (b[field] as any) - (a[field] as any);
-            }
-        });
-    }
-
-    const handleSortByField = (
-        field: keyof (typeof itemsArray)[0],
-        ascending = true
-    ) => {
-        let availableItems = itemsArrState.filter((item) => item.inStock);
-        let unavailableItems = itemsArrState.filter((item) => !item.inStock);
-
-        const sortedAvailableItems = sortByField(
-            availableItems,
-            field,
-            ascending
-        );
-        const sortedUnavailableItems = sortByField(
-            unavailableItems,
-            field,
-            ascending
-        );
-
-        const sortedItems = [
-            ...sortedAvailableItems,
-            ...sortedUnavailableItems,
-        ];
-
-        dispatch(setItemsState(sortedItems));
-    };
-
-    const sortByLowerPrice = () => {
-        handleSortByField("queensPrice", true);
-        dispatch(setSortingTitleState("From cheap to expensive"));
-        setSortingState("");
-    };
-    const sortByHighestPrice = () => {
-        handleSortByField("queensPrice", false);
-        dispatch(setSortingTitleState("From expensive to cheap"));
-        setSortingState("");
-    };
-    const sortByPopularity = () => {
-        handleSortByField("popularity", false);
-        dispatch(setSortingTitleState("By popularity"));
-        setSortingState("");
-    };
-    const sortByStock = () => {
-        handleSortByField("inStock", false);
-        dispatch(setSortingTitleState("In stock"));
-        setSortingState("");
-    };
-    const sortByDefault = () => {
-        handleSortByField("id", true);
-        dispatch(setSortingTitleState("Default"));
-        setSortingState("");
-    };
 
     return (
         <main className="main">
@@ -144,69 +41,7 @@ const CollectionPage = (props: Props) => {
                 <div className="header-bg"></div>
                 <div className="container">
                     <PagesNavigation />
-                    <div className="sorting">
-                        <div
-                            className="row sorting-row"
-                            onClick={openSortingList}
-                        >
-                            <p>Sorting: {sortingTitleState}</p>
-                            <div className={`arrow ${sortingState}`}></div>
-                        </div>
-                        <div className={`sorting-list ${sortingState}`}>
-                            <p
-                                className={`${
-                                    sortingTitleState ===
-                                    "From expensive to cheap"
-                                        ? "disable"
-                                        : ""
-                                }`}
-                                onClick={sortByHighestPrice}
-                            >
-                                From expensive to cheap
-                            </p>
-                            <p
-                                className={`${
-                                    sortingTitleState ===
-                                    "From cheap to expensive"
-                                        ? "disable"
-                                        : ""
-                                }`}
-                                onClick={sortByLowerPrice}
-                            >
-                                From cheap to expensive
-                            </p>
-                            <p
-                                className={`${
-                                    sortingTitleState === "By popularity"
-                                        ? "disable"
-                                        : ""
-                                }`}
-                                onClick={sortByPopularity}
-                            >
-                                By popularity
-                            </p>
-                            <p
-                                className={`${
-                                    sortingTitleState === "In stock"
-                                        ? "disable"
-                                        : ""
-                                }`}
-                                onClick={sortByStock}
-                            >
-                                In stock
-                            </p>
-                            <p
-                                className={`${
-                                    sortingTitleState === "Default"
-                                        ? "disable"
-                                        : ""
-                                }`}
-                                onClick={sortByDefault}
-                            >
-                                Default
-                            </p>
-                        </div>
-                    </div>
+                    <SortingComponent />
                     <div className="tablet-buttons-row row">
                         <div className="tablet-button filters-btn">
                             <p>Filters</p>
